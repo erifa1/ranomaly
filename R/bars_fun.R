1ddc15dd83b78b1c927a7ecce17b65cc403a91ca
Etienne Rifa <etienne.rifa@insa-toulouse.fr>
2021-02-10 17:37:56 +0100

    update bars_fun switch to Ord1 argument to split plot no subplot when Fact1 unique length is equal to nsamples(data)

--- R/bars_fun.R
+++ R/bars_fun.R
@@ -37,9 +37,9 @@ rarefaction <- function(data = data, col = NULL, step = 100, ggplotly = TRUE){
 #' @param data a phyloseq object (output from decontam or generate_phyloseq)
 #' @param rank Taxonomy rank to merge features that have same taxonomy at a certain taxonomic rank (among rank_names(data), or 'ASV' for no glom)
 #' @param top Number of top taxa to plot
-#' @param Ord1 Variable used to order sample (X axis)
-#' @param Fact1 Variable used to change X axis tick labels and color
-#' @param split if TRUE make a facet_wrap like grouped by Fact1
+#' @param Ord1 Variable used to order sample (X axis) or split the barplot if split = TRUE
+#' @param Fact1 Variable used to change X axis tick labels and color (when split = FALSE)
+#' @param split if TRUE make a facet_wrap like grouped by Ord1 (default FALSE)
 #' @param relative Plot relative (TRUE, default) or raw abundance plot (FALSE)
 #'
 #' @return Returns barplots in an interactive plotly community plot
@@ -127,16 +127,20 @@ bars_fun <- function(data = data, rank = "Genus", top = 10, Ord1 = NULL, Fact1 =
     p1=plot_ly(meltdat, x = ~sample.id, y = ~value, type = 'bar', name = ~variable, color = ~variable) %>% #, color = ~variable
       layout(title="Relative abundance", yaxis = list(title = 'Relative abundance'), xaxis = xform, barmode = 'stack')
 
-    p1 <- subplot(p1, subp1, nrows = 2, shareX = T, heights=c(0.95,0.05)) %>%
+    if(length(df1$x) != length(unique(df1$g))){
+      p1 <- subplot(p1, subp1, nrows = 2, shareX = T, heights=c(0.95,0.05)) %>%
       layout(xaxis = xform)
+    }
   }else{
     #raw abundance
     plottitle = "Raw abundance"
     p1=plot_ly(meltdat, x = ~sample.id, y = ~value, type = 'bar', name = ~variable, color = ~variable) %>% #, color = ~variable
       layout(title="Raw abundance", yaxis = list(title = 'Raw abundance'), xaxis = xform, barmode = 'stack')
 
-    p1 <- subplot(p1, subp1, nrows = 2, shareX = T, heights=c(0.95,0.05)) %>%
+    if(length(df1$x) != length(unique(df1$g))){
+      p1 <- subplot(p1, subp1, nrows = 2, shareX = T, heights=c(0.95,0.05)) %>%
       layout(xaxis = xform)
+    }
   }
 
   # dir.create(outpath, recursive = TRUE)
@@ -149,21 +153,21 @@ bars_fun <- function(data = data, rank = "Genus", top = 10, Ord1 = NULL, Fact1 =
   if(!split) {
     return(p1)
   } else {
-    p1 = meltdat %>% group_by(across({Fact1})) %>%
+    p1 = meltdat %>% group_by(across({Ord1})) %>%
       dplyr::group_map(~ plot_ly(data=., x = ~sample.id, y = ~value, type = 'bar',
                                  name = ~variable,
                                  color = ~variable, legendgroup = ~variable,
-                                 showlegend = (.y == levels(meltdat[, Fact1])[1])),
+                                 showlegend = (.y == levels(meltdat[, Ord1])[1])),
                        keep = TRUE)  %>%
       subplot(nrows = 1, shareX = TRUE, shareY=TRUE, titleX = FALSE) %>%
       layout(title=plottitle,
-             xaxis = list(title = glue("{Fact1} = {unique(meltdat[, Fact1])[1]}")),
+             xaxis = list(title = glue("{Ord1} = {unique(meltdat[, Ord1])[1]}")),
              yaxis = list(title = 'Relative abundance'),
              barmode = 'stack')
 
-    for (i in 2:length(unique(meltdat[, Fact1]))) {
+    for (i in 2:length(unique(meltdat[, Ord1]))) {
       p1$x$layoutAttrs[[1]][[paste0("xaxis", i)]] = NULL
-      p1$x$layoutAttrs[[1]][[paste0("xaxis", i)]]$title <- glue("{Fact1} = {unique(meltdat[, Fact1])[i]}")
+      p1$x$layoutAttrs[[1]][[paste0("xaxis", i)]]$title <- glue("{Ord1} = {unique(meltdat[, Ord1])[i]}")
     }
     return(p1)
   }
