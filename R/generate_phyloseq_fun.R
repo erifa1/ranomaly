bff8d692bd0858d3649f2b77731a4d9e2bc93d15
Etienne Rifa <etienne.rifa[at]insa-toulouse.fr>
2021-07-13 12:26:21 +0200

    fix taxonomy integration

--- R/generate_phyloseq_fun.R
+++ R/generate_phyloseq_fun.R
@@ -53,7 +53,7 @@ generate_phyloseq_fun <- function(dada_res = dada_res, tax.table = tax.table, tr
 
   if(is.null(tree)){
     flog.info('Building phyloseq object without tree...')
-    data <- phyloseq(dada_res$otu.table, tax.table, sample.metadata, DNAStringSet(sequences))
+    data <- phyloseq(dada_res$otu.table, tax_table(as.matrix(tax.table)), sample.metadata, DNAStringSet(sequences))
   } else{
     flog.info('Building phyloseq object with tree...')
     data <- phyloseq(dada_res$otu.table, tax.table, sample.metadata, phy_tree(tree), DNAStringSet(sequences))
