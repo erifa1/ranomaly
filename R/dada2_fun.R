76d596cc0273279a96a3bedd1c9fb84a5bc67144
Etienne Rifa <etienne.rifa[at]insa-toulouse.fr>
2021-11-05 10:19:03 +0100

    dada2_fun add extension argument to inform R1 R2 pattern for paired data change amplicon argument to "cutadapt". set to TRUE and inform sequence primers to trim primers with cutadapt. add cutadapt step in singleend SOP (illumina/torrent).

--- R/dada2_fun.R
+++ R/dada2_fun.R
@@ -12,13 +12,14 @@
 #' @param r_primer Reverse primer sequence (only for ITS)
 #' @param plot Plot all test or not
 #' @param compress Reads files are compressed (.gz)
+#' @param extension Paired reads extension files for R1. (default: _R1.fastq), use "_R1.fastq.gz" for compressed files.
 #' @param verbose Verbose level. (1: quiet, 3: verbal)
 #' @param torrent_single Boolean to choose between Illumina Paired End SOP or Torrent Single End SOP. default: FALSE
-#' @param trim_l Trim left size.
-#' @param trim_r Trim right size.
+#' @param trim_l Trim left size, to trim forward primer without cutadapt.
+#' @param trim_r Trim right size, to trim reverse primer without cutadapt.
 #' @param returnval Boolean to return values in console or not.
 #' @param paired Boolean for Illumina Paired End Reads.
-#' @param orient_torrent Forward primer sequence to orient all reads to same strand.
+#' @param orient_torrent Forward primer sequence to orient all reads to same strand (only unambiguous nucleotides).
 #'
 #' @return Return raw otu table in phyloseq object and export it in an Rdata file.
 #'
@@ -32,13 +33,14 @@
 #' @import phyloseq
 #' @importFrom tibble rownames_to_column
 #' @importFrom dplyr left_join
+#' @importFrom stringr str_remove str_replace
 #' @export
 
 # DADA2 function
 
-dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_trunclen = 240, r_trunclen = 240, dadapool = "pseudo",
-                      f_primer = "GCATCGATGAAGAACGCAGC", r_primer = "TCCTCCGCTTWTTGWTWTGC", plot = FALSE, compress = FALSE, verbose = 1,
-                      torrent_single = FALSE,returnval = TRUE, paired = TRUE, trim_l=15, trim_r=0, orient_torrent = NULL){
+dada2_fun <- function(path = "", outpath = "./dada2_out/", cutadapt = FALSE, f_trunclen = 240, r_trunclen = 240, dadapool = "pseudo",
+                      f_primer = "GCATCGATGAAGAACGCAGC", r_primer = "TCCTCCGCTTWTTGWTWTGC", plot = FALSE, compress = FALSE, extension = "_R1.fastq",
+                      verbose = 1, torrent_single = FALSE,returnval = TRUE, paired = TRUE, trim_l=15, trim_r=0, orient_torrent = NULL){
   if(torrent_single == TRUE & is.null(orient_torrent)){stop("Need forward primer to orient TORRENT reads...")}
 
   if(verbose == 3){
@@ -56,26 +58,39 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
   }
   flog.info('Done.')
 
+  flog.info("Extension...")
+  extension2 = stringr::str_replace(extension, "1", "2")
+
+
   if(paired == TRUE){
     flog.info('###ILLUMINA PAIRED END SOP')
 
-    if(compress==TRUE){
-      flog.info('Loading compress files...')
-      fnFs <- sort(list.files(path, pattern = "_R1.fastq.gz", full.names = TRUE))
-      fnRs <- sort(list.files(path, pattern = "_R2.fastq.gz", full.names = TRUE))
-    }else{
-      flog.info('Loading flat files...')
-      fnFs <- sort(list.files(path, pattern = "_R1.fastq$", full.names = TRUE))
-      fnRs <- sort(list.files(path, pattern = "_R2.fastq$", full.names = TRUE))
-    }
+  if(compress==TRUE & length(grep(".gz", extension)) == 0){
+    stop("If argument compress = TRUE, please add '.gz' to extension argument.")
+  }
+
+    flog.info('Loading files...')
+    fnFs <- sort(list.files(path, pattern = extension, full.names = TRUE))
+    fnRs <- sort(list.files(path, pattern = extension2, full.names = TRUE))
+
+
 
     flog.debug("File list...")
     flog.debug(length(fnFs))
     flog.debug(length(fnRs))
     flog.info('Done.')
 
-    if(amplicon=="ITS"){
-      flog.info('DADA2 ITS')
+    if(plot){
+      flog.info('Quality plot ...')
+      pf <- plotQualityProfile(fnFs[1:2])
+      pr <- plotQualityProfile(fnRs[1:2])
+      ggsave(paste(outpath,'/qual_plot_f.png',sep=''), plot=pf)
+      ggsave(paste(outpath,'/qual_plot_r.png',sep=''), plot=pr)
+      flog.info('Done.')
+    }
+
+    if(cutadapt){
+            flog.info(glue::glue('DADA2 Trim primers with cutadapt based on primers sequences ...'))
 
       # Primers sequences
       FWD <- f_primer
@@ -121,11 +136,12 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
 
       flog.info('Primer hits: ')
 
-      rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.filtN[[1]]),
+      primerhits <- rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.filtN[[1]]),
       FWD.ReverseReads = sapply(FWD.orients, primerHits, fn = fnRs.filtN[[1]]),
       REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.filtN[[1]]),
       REV.ReverseReads = sapply(REV.orients, primerHits, fn = fnRs.filtN[[1]])
       )
+      print(primerhits)
 
       flog.info('Done.')
       # Remove primers
@@ -146,7 +162,9 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
         # Trim REV and the reverse-complement of FWD off of R2 (reverse reads)
         R2.flags <- paste("-G", REV, "-A", FWD.RC)
         # Run Cutadapt
+        pb <- txtProgressBar(min = 0, max = length(fnFs), style = 3)
         for(i in seq_along(fnFs)) {
+          setTxtProgressBar(pb, i)
           if(verbose == 3){
             system2(cutadapt, args = c(R1.flags, R2.flags, "-n", 2, # -n 2 required to remove FWD and REV from reads
             "-o", fnFs.cut[i], "-p", fnRs.cut[i], # output files
@@ -157,6 +175,7 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
             fnFs.filtN[i], fnRs.filtN[i]), stdout=NULL, stderr=NULL) # input files
           }
         }
+        close(pb)
       } else{
         flog.info('Cutadapt files exists. Skipping...')
       }
@@ -164,14 +183,15 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
       flog.info('Done.')
       # Sanity check
       flog.info('Primer left: ')
-      rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.cut[[1]]),
+      primerhits2 <- rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.cut[[1]]),
       FWD.ReverseReads = sapply(FWD.orients, primerHits, fn = fnRs.cut[[1]]),
       REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.cut[[1]]),
       REV.ReverseReads = sapply(REV.orients, primerHits, fn = fnRs.cut[[1]]))
+      print(primerhits2)
 
       # Forward and reverse fastq filenames have the format:
-      cutFs <- sort(list.files(path.cut, pattern = "_R1.fastq", full.names = TRUE))
-      cutRs <- sort(list.files(path.cut, pattern = "_R2.fastq", full.names = TRUE))
+      cutFs <- sort(list.files(path.cut, pattern = extension, full.names = TRUE))
+      cutRs <- sort(list.files(path.cut, pattern = extension2, full.names = TRUE))
 
       # Extract sample names, assuming filenames have format:
 
@@ -190,30 +210,26 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
       names(filtRs) <- sample.names
 
       flog.debug(length(cutFs))
-      print(head(cutFs))
       flog.debug(length(filtFs))
-      print(head(filtFs))
-      flog.debug(length(cutRs))
-      print(head(cutRs))
+      flog.debug(length(cutRs))      
       flog.debug(length(filtRs))
-      print(head(filtRs))
+      
 
-      out <- filterAndTrim(cutFs, filtFs, cutRs, filtRs, maxN = 0, maxEE = c(2, 2),
-      truncQ = 2, minLen = 50, compress = FALSE, multithread = TRUE)  # on windows, set multithread = FALSE
+      out0 <- filterAndTrim(cutFs, filtFs, cutRs, filtRs, maxN = 0, maxEE = c(2, 2),
+      truncQ = 2, minLen = 50, compress = compress, multithread = TRUE)  # on windows, set multithread = FALSE
       #head(out)
+      row.names(out0) = sample.names
+      out <- as.data.frame(out0) %>%tibble::rownames_to_column(var = "sample.id")
 
-    } else if(amplicon=="16S"){
 
-      flog.info('DADA2 16S')
-      # save.image("debug.rdata")
-      if(plot){
-        flog.info('Plotting 1 ...')
-        pf <- plotQualityProfile(fnFs[1:2])
-        pr <- plotQualityProfile(fnRs[1:2])
-        ggsave(paste(outpath,'/qual_plot_f.png',sep=''), plot=pf)
-        ggsave(paste(outpath,'/qual_plot_r.png',sep=''), plot=pr)
-        flog.info('Done.')
-      }
+      filtFs_out <- grep("F_filt",list.files(glue::glue("{path}/filtered/"), full.names = TRUE), value = TRUE)
+      filtRs_out <- grep("R_filt",list.files(glue::glue("{path}/filtered/"), full.names = TRUE), value = TRUE)
+
+      # samples_names <- grep(".fastq",list.files("./data_arch_links/", full.names = FALSE), value = TRUE)
+
+    } else {
+
+      flog.info(glue::glue('DADA2 Trim primers on {trim_l} bases ...'))
 
       flog.debug(length(fnFs))
       flog.debug(length(fnRs))
@@ -247,11 +263,10 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
       filtFs_out <- grep("F_filt",list.files(glue::glue("{path}/filtered/"), full.names = TRUE), value = TRUE)
       filtRs_out <- grep("R_filt",list.files(glue::glue("{path}/filtered/"), full.names = TRUE), value = TRUE)
 
-      samples_names <- grep(".fastq",list.files("./data_arch_links/", full.names = FALSE), value = TRUE)
-
+      # samples_names <- grep(".fastq",list.files(path, full.names = FALSE), value = TRUE)
+      # print(samples_names)
 
       flog.info('Done.')
-
     }
     
     #COMMON
@@ -335,7 +350,6 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
     dada_res$seqtab.nochim = seqtab.nochim
     dada_res$seqtab.export = seqtab.export
     dada_res$otu.table = otu.table
-
     flog.info('Saving R objects.')
     save(dada_res, file=paste(outpath,'/robjects.Rdata',sep=''))
     flog.info('Finish.')
@@ -361,14 +375,151 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
       filtFs <- file.path(path, "filtered", paste0(sample.names, "_filt.fastq"))
     }
 
-    if(torrent_single == TRUE){
-      out <- filterAndTrim(fwd = fnFs, filt = filtFs, maxN = 0, multithread = TRUE, verbose=TRUE, rm.phix = TRUE,
-        , maxEE = 5 , minLen = 100, compress=compress, trimLeft=trim_l, trimRight=trim_r, orient.fwd = orient_torrent)
+    # Cutadapt
+
+      if(cutadapt){
+          flog.info(glue::glue('DADA2 Trim primers with cutadapt based on primers sequences ...'))
+
+      # Primers sequences
+      FWD <- f_primer
+      REV <- r_primer
+
+      # Generate all primers orientations
+      allOrients <- function(primer) {
+        # Create all orientations of the input sequence
+        # require(Biostrings)
+        dna <- DNAString(primer)  # The Biostrings works w/ DNAString objects rather than character vectors
+        orients <- c(Forward = dna, Complement = IRanges::reverse(reverseComplement(dna)), Reverse = IRanges::reverse(dna),  # bug with complement() function
+        RevComp = reverseComplement(dna))
+        return(sapply(orients, toString))  # Convert back to character vector
+      }
+      flog.info('Primer orientation')
+      FWD.orients <- allOrients(FWD)
+      REV.orients <- allOrients(REV)
+      #FWD.orients
+
+      flog.info('Remove Ns from reads')
+      # Remove Ns from reads
+      fnFs.filtN <- file.path(path, "filtN", basename(fnFs)) # Put N-filterd files in filtN/ subdirectory
+
+
+      # print(fnFs.filtN)
+
+      flog.info('filterAndTrim...')
+      if(! dir.exists(paste(path,'/filtN',sep=''))){
+        filterAndTrim(fwd = fnFs, filt = fnFs.filtN, maxN = 0, multithread = TRUE, verbose=TRUE, rm.phix = TRUE, compress=compress)
+      }else{
+        flog.info('Filtered files exist, skipping...')
+      }
+
+      flog.info('Done.')
+
+      # Search primers in reads.
+      primerHits <- function(primer, fn) {
+        # Counts number of reads in which the primer is found
+        nhits <- vcountPattern(primer, sread(readFastq(fn)), fixed = FALSE)
+        return(sum(nhits > 0))
+      }
+
+      flog.info('Primer hits: ')
+
+      primerhits <- rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.filtN[[1]]),
+      REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.filtN[[1]])
+      )
+      print(primerhits)
+
+      flog.info('Done.')
+      # Remove primers
+      flog.info('Runnning cutadapt...')
+
+      cutadapt <- system("which cutadapt", intern=TRUE)   # CHANGE ME to the cutadapt path on your machine
+      flog.debug(cutadapt)
+      path.cut <- file.path(path, "cutadapt")
+      fnFs.cut <- file.path(path.cut, basename(fnFs))
+      flog.debug(head(fnFs))
+
+      if(!dir.exists(path.cut)){
+        dir.create(path.cut)
+        FWD.RC <- dada2:::rc(FWD)
+        REV.RC <- dada2:::rc(REV)
+        # Trim FWD and the reverse-complement of REV off of R1 (forward reads)
+        R1.flags <- paste("-g", FWD, "-a", REV.RC)
+
+        # Run Cutadapt
+        pb <- txtProgressBar(min = 0, max = length(fnFs), style = 3)
+        for(i in seq_along(fnFs)) {
+          setTxtProgressBar(pb, i)
+          if(verbose == 3){
+            system2(cutadapt, args = c(R1.flags, "-n", 2, # -n 2 required to remove FWD and REV from reads
+            "-o", fnFs.cut[i], # output files
+            fnFs.filtN[i]), stdout="", stderr="") # input files
+          } else{
+            system2(cutadapt, args = c(R1.flags, "-n", 2, # -n 2 required to remove FWD and REV from reads
+            "-o", fnFs.cut[i], # output files
+            fnFs.filtN[i]), stdout=NULL, stderr=NULL) # input files
+          }
+        }
+        close(pb)
+      } else{
+        flog.info('Cutadapt files exists. Skipping...')
+      }
+
+      flog.info('Done.')
+      # Sanity check
+      flog.info('Primer left: ')
+      primerhits2 <- rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.cut[[1]]),
+      REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.cut[[1]]))
+      print(primerhits2)
+
+      if(compress){
+        cutFs <- sort(list.files(path.cut, pattern = ".fastq.gz", full.names = TRUE))
+      }else{
+        cutFs <- sort(list.files(path.cut, pattern = ".fastq", full.names = TRUE))
+      }
+
+      sample.names <- unname(sapply(cutFs, get.sample.name))
+
+      if(compress){
+        filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
+      }else{
+        filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq"))
+      }
+
+      names(filtFs) <- sample.names
+      print(filtFs)
+
+      flog.debug(length(cutFs))
+      flog.debug(length(filtFs))
+      
+      if(torrent_single == TRUE){
+      out0 <- filterAndTrim(fwd = cutFs, filt = filtFs, maxN = 0, multithread = FALSE, rm.phix = TRUE,
+        , maxEE = 5 , minLen = 100, compress=compress, orient.fwd = orient_torrent)
+      }else{
+      out0 <- filterAndTrim(fwd = cutFs, filt = filtFs, maxN = 0, multithread = TRUE, rm.phix = TRUE,
+        , maxEE = 5 , minLen = 100, compress=compress)
+      }
+
+      row.names(out0) = sample.names
+      out <- as.data.frame(out0) %>%tibble::rownames_to_column(var = "sample.id")
+
+
+      filtFs_out <- grep("F_filt",list.files(glue::glue("{path}/filtered/"), full.names = TRUE), value = TRUE)
+
+      # samples_names <- grep(".fastq",list.files("./data_arch_links/", full.names = FALSE), value = TRUE)
+
     }else{
-      out <- filterAndTrim(fwd = fnFs, filt = filtFs, maxN = 0, multithread = TRUE, verbose=TRUE, rm.phix = TRUE,
-        , maxEE = 5 , minLen = 100, compress=compress, trimLeft=trim_l, trimRight=trim_r )
+      flog.info(glue::glue('DADA2 Trim primers on {trim_l}(FWD) and {trim_r}(REV) bases ...'))
+      if(torrent_single == TRUE){
+        out <- filterAndTrim(fwd = fnFs, filt = filtFs, maxN = 0, multithread = TRUE, verbose=TRUE, rm.phix = TRUE,
+          , maxEE = 5 , minLen = 100, compress=compress, trimLeft=trim_l, trimRight=trim_r, orient.fwd = orient_torrent)
+      }else{
+        out <- filterAndTrim(fwd = fnFs, filt = filtFs, maxN = 0, multithread = TRUE, verbose=TRUE, rm.phix = TRUE,
+          , maxEE = 5 , minLen = 100, compress=compress, trimLeft=trim_l, trimRight=trim_r )
+      }
+      row.names(out) = sample.names
+      out <- as.data.frame(out0) %>%tibble::rownames_to_column(var = "sample.id")
+
     }
-    row.names(out) = sample.names
 
     flog.info('Learning error model...')
     errF <- learnErrors(filtFs, multithread=TRUE)
@@ -381,9 +532,8 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
       flog.info('Done.')
     }
 
-    mergers <- vector("list", length(sample.names))
-    names(mergers) <- sample.names;
-    stockFs=NULL; stockRs=NULL
+
+    stockFs=NULL
     getN <- function(x) sum(getUniques(x))
 
 
@@ -425,14 +575,15 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
       track <- c(out, sum(stockFs), rowSums(seqtab.nochim)[1])
       names(track) <- c("input", "filtered", "denoisedF", "nonchim")
     }else{
-      nn = row.names(out)
-      print(rownames(stockFs))
+      rownames(out) <- out$sample.id
+      nn <- row.names(out)
+      print(names(stockFs))
       track <- cbind.data.frame(out, stockFs[nn], rowSums(seqtab.nochim)[nn])
-      colnames(track) <- c("input", "filtered", "denoisedF", "nonchim")
+      colnames(track) <- c("sample.id","input", "filtered", "denoisedF", "nonchim")
       head(track)
     }
-    # If processing a single sample, remove the sapply calls: e.g. replace sapply(dadaFs, getN) with getN(dadaFs)
-    write.table(track, paste(outpath,"/read_tracking.csv",sep=''), sep="\t", row.names=TRUE, col.names=NA, quote=FALSE)
+    flog.info('Writing table ...')
+    write.table(track, paste(outpath,"/read_tracking.csv",sep=''), sep="\t", row.names=FALSE, quote=FALSE)
 
     seqtab.export <- seqtab.nochim
     colnames(seqtab.export) <- sapply(colnames(seqtab.export), digest::digest, algo="md5")
@@ -453,9 +604,9 @@ dada2_fun <- function(amplicon = "16S", path = "", outpath = "./dada2_out/", f_t
     flog.info('Saving R objects.')
     save(dada_res, file=paste(outpath,'/robjects.Rdata',sep=''))
 
+
     flog.info('Finish.')
     if(returnval) {return(dada_res)}
-
 
 
   }
